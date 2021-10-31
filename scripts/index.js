6a0ed6df8d5cd48a7c50b1bbb750e6e75d1fdef3
MarkTropoja <93010175+MarkTropoja@users.noreply.github.com>
2021-10-31 02:06:38 +0100

    case-simulator-added

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,10 @@ let input1 = document.getElementById("input1");
 let input2 = document.getElementById("input2"); 
 let result = document.getElementById("result");
 let spyArray = [];
+let caseImage = document.getElementById("case-img");
+const NUMS_OF_ITEMS = 6;
+
+
 
 //-gets input from index.html input
 function getInput(){
@@ -46,3 +50,24 @@ function spy(num){
     spyArray.push(num);
 }
 
+
+//-cases
+function caseOpening(){
+
+    let randomNumber = Math.random();
+    let imageNumber = Math.ceil(NUMS_OF_ITEMS * Math.random());
+    
+    console.log(randomNumber); 
+
+    if (randomNumber > 0.15){
+        console.log("enlisted");
+        caseImage.src = "./assets/images/enlisted/" + imageNumber + ".jpg";
+    }
+
+    else {
+        console.log("elite");
+        caseImage.src = "./assets/images/elite/" + imageNumber + ".jpg";
+    }
+
+    setTimeout(()=> {caseImage.src = "./assets/images/aw-symbol.png" }, 3000);
+}
